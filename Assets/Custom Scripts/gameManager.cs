334aa04dddf7eb4b18ea63a08817454f00fcba16
mliu195 <mliu195@uottawa.ca>
2023-11-27 22:25:20 -0500

    Scoring System
    -Modifed gameManager to detect when a book was in the socket, increase score, and then disable said book. This was the player can add lots of books to this location and increase their score everytime.
    -Deleted "socketHighlighter
    -Deleted Sphere collider component of debug book
    -yeah i had an idea of using simple interactables with books as the interacters to keep score didn't work since it wasn't built to be like that, so i got rid of all the components for that (the above 2). Now we're using sockets to keep track of the score.
    -If you guys have any questions be sure to let me know

--- Assets/Custom Scripts/gameManager.cs
+++ Assets/Custom Scripts/gameManager.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class gameManager : MonoBehaviour
 {
     /// <summary>
     /// The max amount of books the player must get (>=1)
     /// </summary>
-    public int winScore;
+    public int maxScore;
+    /// <summary>
+    /// The nuber of books a player got
+    /// </summary>
     private int score;
-    public UnityEvent maxScoreReached;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,18 +24,57 @@ public class gameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (score >= winScore)
+        //Check for new books
+        checkBook();
+        ///Check Win
+        checkWin();
+    }
+
+    /// <summary>
+    /// Invoked when player has got the max number oof books allowed
+    /// </summary>
+    public UnityEvent maxScoreReached;
+    /// <summary>
+    /// Checks if the number of books has exceeded its maximum and invokes an event if it does.
+    /// </summary>
+    private void checkWin()
+    {
+        if (score >= maxScore)
         {
             maxScoreReached.Invoke();
         }
     }
-
-    public void incrament()
+    /// <summary>
+    /// The socket player put the books in
+    /// </summary>
+    [SerializeField, Tooltip("The Socket that should be checked for books.")]
+    private XRSocketInteractor socket;
+    /// <summary>
+    /// Increases score if the player found a book, and then disables the book
+    /// </summary>
+    private void checkBook()
     {
-        score++;
+        if (socket.hasSelection)
+        {
+            score++;
+            GameObject book = socket.GetOldestInteractableSelected().transform.gameObject;
+            book.SetActive(false);
+            Debug.Log("score got!");
+        }
     }
-    public void decrament()
+
+    /// <summary>
+    /// Sets number of books player has found so far
+    /// </summary>
+    public void setScore(int score)
     {
-        score--;
+        this.score = score;
+    }
+    /// <summary>
+    /// Gets number of books player has found so far
+    /// </summary>
+    public int getScore()
+    {
+        return score;
     }
 }
